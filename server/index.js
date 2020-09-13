f59817cfc331729264682ea2d6d69bd1a273318a
Langster2323 <langahkeem@gmail.com>
2020-09-13 15:06:18 -0500

    installed morgan

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const cors = require('cors')
+const cors = require('cors');
+const logger = require('morgan');
 const home = require('./routes/home');
 const app = express();
 
 app.use(cors());
+app.use(logger());
 
 app.use(express.json());
 app.use('/', home)
