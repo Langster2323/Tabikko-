91245f51526aa0e2ac93a6ba65c71f8d5c789dbe
Langster2323 <langahkeem@gmail.com>
2020-09-13 14:58:58 -0500

    installed cors and axios

--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const cors = require('cors')
 const home = require('./routes/home');
 const app = express();
 
+app.use(cors());
 
 app.use(express.json());
 app.use('/', home)
