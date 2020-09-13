f06c55f3ae692167738cc759970c10758b7bbe0e
Langster2323 <langahkeem@gmail.com>
2020-09-13 16:50:17 -0500

    connected to database. connecting backend and front end

--- client/tabikko/src/App.js
+++ client/tabikko/src/App.js
@@ -1,25 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
 
 const App = () => {
-
+  const [data, setData] = useState([]);
+  useEffect( () => {
+    axios({
+      method: "GET",
+      url: 'http://localhost:3200/'
+    }).then(res => {
+      setData(res.data)
+    })
+  }, [setData])
+  console.log(data);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          {data}
+        </div>
       </header>
     </div>
   );
--- server/index.js
+++ server/index.js
@@ -1,16 +1,27 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
+
 const logger = require('morgan');
 const home = require('./routes/home');
+
+require('dotenv').config();
+
 const app = express();
+const port = process.env.PORT || 9000;
 
+app.use(logger('dev'));
+
+//Middleware
 app.use(cors());
-app.use(logger());
-
 app.use(express.json());
 app.use('/', home)
 
-
-const port = process.env.port || 3200;
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true,  useUnifiedTopology: true });
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log("MongoDB database connection established successfully");
+})
 
 app.listen(port, () => {console.log(`Port is running on ${port}`);})
