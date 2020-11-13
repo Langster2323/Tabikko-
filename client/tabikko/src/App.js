dfbf180675ae5ddc4d39b14b1afdbb4f6352f9b9
Langster2323 <langahkeem@gmail.com>
2020-11-12 20:10:49 -0600

    began creating skeleton components.

--- client/tabikko/src/App.js
+++ client/tabikko/src/App.js
@@ -1,26 +1,36 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import axios from 'axios';
-import "bootstrap/dist/css/boostrap.min.css"
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import Navbar from './components/navbar.component';
+import ExercisesList from './components/exercises-list.component';
+import EditExercise from './components/edit-exercise.component';
+import CreateExercise from './components/create-exercise.component';
+import CreateUser from './components/create-user.component';
+
 const App = () => {
-  const [data, setData] = useState([]);
-  useEffect( () => {
-    axios({
-      method: "GET",
-      url: 'http://localhost:3200/'
-    }).then(res => {
-      setData(res.data)
-    })
-  }, [setData])
-  console.log(data);
+  // const [data, setData] = useState([]);
+  // useEffect( () => {
+  //   axios({
+  //     method: "GET",
+  //     url: 'http://localhost:3200/'
+  //   }).then(res => {
+  //     setData(res.data)
+  //   }) 
+  // }, [setData])
+  // console.log(data);
   return (
-    <div className="App">
-      <header className="App-header">
-        <div>
-          {data}
-        </div>
-      </header>
-    </div>
+    <Router>
+      <div className="container">
+        <Navbar />
+        <br/>
+        <Route path="/" exact component={ExercisesList} />
+        <Route path="/edit/:id" component={EditExercise} />
+        <Route path="/create" component={CreateExercise} />
+        <Route path="/user" component={CreateUser} />
+      </div>
+    </Router>
   );
 }
 
