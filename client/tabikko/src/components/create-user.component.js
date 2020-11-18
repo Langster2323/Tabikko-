3c7a3e90520313c39143b3ffd7df82e1810e287a
Langster2323 <langahkeem@gmail.com>
2020-11-17 21:09:20 -0600

    can now submit users

--- client/tabikko/src/components/create-user.component.js
+++ client/tabikko/src/components/create-user.component.js
@@ -1,10 +1,50 @@
 import React, { Component } from 'react';
 
 export default class CreateUser extends Component {
+    constructor(props) {
+        super();
+        this.state = {
+            username: ''
+        }
+    }
+
+    onChangeUserName = (e) => {
+        this.setState({
+            username: e.target.value
+        })
+    }
+
+    onSubmit = (e) => {
+        const { username } = this.state;
+        e.preventDefault();
+
+        const user = {
+            username: username
+        }
+
+        console.log(user);
+
+        this.setState({
+            username: ''
+        })
+    }
     render() {
         return (
             <div>
-                <p>You are on the Create Users component!</p>
+                <h3>Create New User</h3>
+                <form onSubmit={this.onSubmit}>
+                    <div className="form-group">
+                        <label>Username: </label>
+                        <input 
+                        type="text" 
+                        required 
+                        className="form-control" 
+                        value={this.state.username} 
+                        onChange={this.onChangeUserName}
+                         />
+                    </div>
+                    <input type="submit" value="Create User" className="btn btn-primary" />
+                </form>
             </div>
         )
     }
